5bfe81dc9c38d14347af37fc555d49955c1c6e6a
Shefa <shefaoudeen123@gmail.com>
2025-02-26 21:35:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Homepage from "./Pages/Homepage";
+import CallforPaper from "./Pages/CallforPaper";
+import PaperSubmission from "./Pages/PaperSubmission";
+import Registration from "./Pages/Registration";
+import Committees from "./Pages/Committees";
+import Contact from "./Pages/Contact";
+import Timeline from "./Pages/Timeline";
+import Speakers from "./Pages/Speakers";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/callforpaper" element={<CallforPaper />} />
+          <Route path="/papersubmission" element={<PaperSubmission />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/committees" element={<Committees />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/speakers" element={<Speakers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
